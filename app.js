b3b277d9da9d84d7929dcb933669e38c185a6486
Farhad32155 <farhadhossain3540@gmail.com>
2022-12-07 13:01:22 +0600

    navbar & card added

---
+++ app.js
@@ -0,0 +1,12 @@
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '61fe7640cdmsh0174675333834cep14e9e4jsn1c8b88e10557',
+        'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+    }
+};
+
+fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Dhaka', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
